9c0198ea0b00e51be0c6d06fbf5790f00536031e
MaximilianoCalabro <maxi_calabro@hotmail.com>
2018-01-14 00:44:42 -0300

    .admin/admin

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,7 +52,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'home';
 $route['404_override'] = 'errors/page_missing';
 $route['translate_uri_dashes'] = FALSE;
-
+$route['.admin'] = 'admin';
 /*
 | -------------------------------------------------------------------------
 | Added by CI Bootstrap 3
